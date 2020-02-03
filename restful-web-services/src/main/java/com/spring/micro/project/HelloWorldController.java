e7e85a9c221fda44765e70339ea139fe4265cbef
Nusrat <njahan8@centennialcollege.ca>
2020-02-02 19:35:12 -0500

    initial commit

---
+++ restful-web-services/src/main/java/com/spring/micro/project/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.spring.micro.project;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class HelloWorldController {
+	
+	//@RequestMapping(method=RequestMethod.GET, path = "")
+	@GetMapping(path = "")
+	public String helloWorld() {
+		
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-World-Bean")
+	public HelloWorldBean helloWorldBean() {
+		//throw new RuntimeException("Some error has happened!!");
+		return new HelloWorldBean("Hello World - Changed");
+	}
+	
+	//hello-World/Path-variable/nusrat
+	@GetMapping(path = "/hello-World/Path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+
+}
---
+++ restful-web-services/src/test/java/com/spring/micro/project/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.micro.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
